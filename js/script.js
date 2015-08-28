c6d2e3dd9ceb0024cc997e1ae9b49f77cf04f7f4
Peter John Saliente <psaliente@users.noreply.github.com>
2015-08-28 17:48:28 +0800

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$( document ).ready( function() {
+ 	 $(".button-collapse").sideNav();
+ 	 $('select').material_select();
+	  $('.datepicker').pickadate({
+	    selectMonths: true, // Creates a dropdown to control month
+	    selectYears: 15 // Creates a dropdown of 15 years to control year
+	  });
+
+	    $(document).ready(function(){
+	    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+	    $('.modal-trigger').leanModal();
+	  });
+});
+$(function(){
+	var operation = "A"; //"A"=Adding;
+	var selected_index = -1;
+	var tbEvents = localStorage.getItem("tbEvents");
+
+	tbEvents = JSON.parse(tbEvents);
+	if(tbEvents == null) 
+		tbEvents = [];
+});
