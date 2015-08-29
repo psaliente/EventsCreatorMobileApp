49f8875a149a6151662a38608f64616c503ec7a5
Peter John Saliente <psaliente@users.noreply.github.com>
2015-08-29 09:50:17 +0000

    Update script.js
    fixed syntax error

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ $( document ).ready( function() {
  	 $('select').material_select();
 	  $('.datepicker').pickadate({
 	    selectMonths: true, // Creates a dropdown to control month
-	    selectYears: 15 // Creates a dropdown of 15 years to control year
-	    min: new Date()
+	    selectYears: 15 ,// Creates a dropdown of 15 years to control year
+	    min: new Date() //past is past, move on na.
 	  });
 
 	    $(document).ready(function(){
