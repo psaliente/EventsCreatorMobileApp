ac5b94345e16e817f6f1f04e1693068de8e47c56
Peter John Saliente <psaliente@users.noreply.github.com>
2015-09-01 17:39:54 +0800

    Added error handling, set initial checklist values

--- js/viewmodel.index.js
+++ js/viewmodel.index.js
@@ -7,6 +7,7 @@ function IndexViewModel(){
 	self.EventDate = ko.observable();
 	self.Category = ko.observable();
 	self.Venue = ko.observable();
+	self.ErrorMessage = ko.observable();
 	//public arrays
 	self.Categories = [{id:"1", name:"Baby Shower"},
               { id:"2", name:"Christening"},
@@ -16,36 +17,87 @@ function IndexViewModel(){
               { id:"6", name:"Reunion"},
               { id:"7", name:"Conference"}];	
 	self.Events = ko.observableArray([]);
-	//public methods
-	self.RetrieveEvents = function () {
-		IDCounter = parseInt(window.localStorage.getItem('ProjectEventIDCounter')) || 0;
-		var parsedData = JSON.parse(window.localStorage.getItem('ProjectEvents')) || [];
-		self.Events(parsedData);
-	}
-	self.SaveEvent = function(){
+	self.Supplies = ko.observableArray([]);
+	self.Foods = ko.observableArray([]);
+	self.Services = ko.observableArray([]);
+	//private methods
+	var SetInitialList = function (eventID) {
 		try{
-			var tmpEvent = {
-				EventID: ++IDCounter,
-				EventName: self.EventName(),
-				EventDate: new Date(self.EventDate()),
-				Category: self.Category(),
-				Venue: self.Venue()
-			};
-			self.Events.push(tmpEvent);
+			var _id = parseInt(self.Category()) - 1;
+			ko.utils.arrayForEach(PreSelectedValues[_id]["InitialSupplies"], function(supply) {
+				var tmpSupply = new Supply($.extend({},supply,{EventID: eventID}));
+				self.Supplies.push(tmpSupply);
+			});
+			ko.utils.arrayForEach(PreSelectedValues[_id]["InitialFoods"], function(food) {
+				var tmpFood = new Food($.extend({},food,{EventID: eventID}));
+				self.Foods.push(tmpFood);
+			});
+			ko.utils.arrayForEach(PreSelectedValues[_id]["InitialServices"], function(service) {
+				var tmpService = new Supply($.extend({},service,{EventID: eventID}));
+				self.Services.push(tmpService);
+			});
+			self.ErrorMessage(null);
+		} 
+		catch (ex) { 
+			self.ErrorMessage("Error! " + ex + ", please contact developer."); 
+		}
+	};
+	//public methods
+	self.SaveEvent = function () {
+		var tmpEvent = {
+			EventID: ++IDCounter,
+			EventName: self.EventName(),
+			EventDate: new Date(self.EventDate()),
+			Category: self.Category(),
+			Venue: self.Venue()
+		};
+		self.Events.push(tmpEvent);
+		SetInitialList(tmpEvent.EventID);
+		self.UpdateLocalStorage();
+	};
+	self.LoadFromStorage = function () {
+		IDCounter = parseInt(window.localStorage.getItem('ProjectEventIDCounter')) || 0;
+		var parsedEvents = JSON.parse(window.localStorage.getItem('ProjectEvents')) || [];
+		var parsedSupplies = JSON.parse(window.localStorage.getItem('EventSupplies')) || [];
+		var parsedFoods = JSON.parse(window.localStorage.getItem('EventFoods')) || [];
+		var parsedServices = JSON.parse(window.localStorage.getItem('EventServices')) || [];
+		var mappedSupplies = $.map(parsedSupplies, function(supply){
+			return new Supply(supply);
+		});
+		var mappedFoods = $.map(parsedFoods, function(food){
+			return new Food(food);
+		});
+		var mappedServices = $.map(parsedServices, function(service){
+			return new Service(service);
+		});
+		self.Events(parsedEvents);
+		self.Supplies(mappedSupplies);
+		self.Foods(mappedFoods);
+		self.Services(mappedServices);
+	};
+	self.UpdateLocalStorage = function(){
+		try{
 			var jsonData = ko.toJSON(self.Events());
+			var jsonSupplies = ko.toJSON(self.Supplies());
+			var jsonFoods = ko.toJSON(self.Foods());
+			var jsonServices = ko.toJSON(self.Services());
 			window.localStorage.setItem('ProjectEvents',jsonData);
 			window.localStorage.setItem('ProjectEventIDCounter',IDCounter);
+			window.localStorage.setItem("EventSupplies",jsonSupplies);
+			window.localStorage.setItem("EventFoods",jsonFoods);
+			window.localStorage.setItem("EventServices",jsonServices);
 			self.EventName("");
 			self.EventDate("");
 			self.Category("");
 			self.Venue("");
+			self.ErrorMessage(null);
 		}
 		catch (ex){
-			//sasaluhin mo pa ba si ex kung mag-fall syang muli sa'yo?
+			self.ErrorMessage("Error! " + ex + ", please contact developer."); 
 		}
-	}
+	};
 }
 
 var ivm = new IndexViewModel();
-$(document).ready(function(){ko.applyBindings(ivm);ivm.RetrieveEvents()});
+$(document).ready(function(){ko.applyBindings(ivm);ivm.LoadFromStorage()});
 
