8823d4dff5c10e916cf13aa5f681771f75f8974f
PSaliente <peterjohncorderosaliente@gmail.com>
2015-09-03 15:04:33 +0800

    dispose orphaned list items. close #2

--- js/viewmodel.calendar.js
+++ js/viewmodel.calendar.js
@@ -14,9 +14,33 @@ function CalendarViewModel() {
 	//public arrays	
 	self.Events = ko.observableArray([]);
 	self.EventTasks = ko.observableArray([]);
+	self.Guests = ko.observableArray([]);
+	self.Supplies = ko.observableArray([]);
+	self.Foods = ko.observableArray([]);
+	self.Services = ko.observableArray([]);
 	self.Months = [{id:0, monthName: "January"},{id:1, monthName: "February"},{id:2, monthName: "March"},{id:3, monthName: "April"},{id:4, monthName: "May"},{id:5, monthName: "June"},
 	{id:6, monthName: "July"},{id:7, monthName: "August"},{id:8, monthName: "September"},{id:9, monthName: "October"},{id:10, monthName: "November"},{id:11, monthName: "December"}];
 	self.Years = [2015,2016,2017,2018,2019,2020,2021];
+	//private methods
+	var DisposeRelatedItems = function(eventID){
+		var guestsToDel = [], foodsToDel = [], suppliesToDel = [], servicesToDel = [];
+		ko.utils.arrayForEach(self.Guests(), function(item) {
+			if(guest.EventID() == eventID) guestsToDel.push(item);
+		});
+		ko.utils.arrayForEach(self.Supplies(), function(item) {
+			if(guest.EventID() == eventID) suppliesToDel.push(item);
+		});
+		ko.utils.arrayForEach(self.Foods(), function(item) {
+			if(guest.EventID() == eventID) foodsToDel.push(item);
+		});
+		ko.utils.arrayForEach(self.Services(), function(item) {
+			if(guest.EventID() == eventID) servicesToDel.push(item);
+		});
+		ko.utils.arrayForEach(guestsToDel, function(toDel) { self.Guests.remove(toDel); });
+		ko.utils.arrayForEach(suppliesToDel, function(toDel) { self.Supplies.remove(toDel); });
+		ko.utils.arrayForEach(foodsToDel, function(toDel) { self.Foods.remove(toDel); });
+		ko.utils.arrayForEach(servicesToDel, function(toDel) { self.Services.remove(toDel); });
+	};
 	//public methods
 	self.days = ko.computed(function () {
 		var tmpDays = new Array(6), dayCtr = 1;
@@ -115,8 +139,9 @@ function CalendarViewModel() {
 	self.RemoveEvent = function(pEvent) {
 		var _delete = confirm("Are you sure you want to delete the event \"" + pEvent.EventName + "\"?");
 		if(_delete){
-			var _name = pEvent.EventName;
+			var _name = pEvent.EventName, _id = pEvent.EventID;
 			self.Events.remove(pEvent);
+			DisposeRelatedItems(_id);
 			self.UpdateLocalStorage();
 			Materialize.toast(_name + ' has been deleted', 3000);
 		}
@@ -124,18 +149,46 @@ function CalendarViewModel() {
 	self.LoadFromStorage = function () {
 		var parsedEvents = JSON.parse(window.localStorage.getItem('ProjectEvents')) || [];
 		var parsedTasks = JSON.parse(window.localStorage.getItem('ProjectEventTasks')) || [];
+		var parsedGuests = JSON.parse(window.localStorage.getItem('EventGuests')) || [];
+		var parsedSupplies = JSON.parse(window.localStorage.getItem('EventSupplies')) || [];
+		var parsedFoods = JSON.parse(window.localStorage.getItem('EventFoods')) || [];
+		var parsedServices = JSON.parse(window.localStorage.getItem('EventServices')) || [];
 		var mappedTasks = $.map(parsedTasks, function(task) {
 			return new EventTask(task);
 		});
+		var mappedGuests = $.map(parsedGuests, function(guest){
+			return new Guest(guest);
+		});
+		var mappedSupplies = $.map(parsedSupplies, function(supply){
+			return new Supply(supply);
+		});
+		var mappedFoods = $.map(parsedFoods, function(food){
+			return new Food(food);
+		});
+		var mappedServices = $.map(parsedServices, function(service){
+			return new Service(service);
+		});
 		self.Events(parsedEvents);
 		self.EventTasks(mappedTasks);
+		self.Guests(mappedGuests);
+		self.Supplies(mappedSupplies);
+		self.Foods(mappedFoods);
+		self.Services(mappedServices);
 	};
 	self.UpdateLocalStorage = function() {
 		try{
 			var jsonTasks = ko.toJSON(self.EventTasks());
 			var jsonEvents = ko.toJSON(self.Events());
+			var jsonGuests = ko.toJSON(self.Guests());
+			var jsonSupplies = ko.toJSON(self.Supplies());
+			var jsonFoods = ko.toJSON(self.Foods());
+			var jsonServices = ko.toJSON(self.Services());
 			window.localStorage.setItem('ProjectEventTasks',jsonTasks);
 			window.localStorage.setItem('ProjectEvents',jsonEvents);
+			window.localStorage.setItem("EventGuests",jsonGuests);
+			window.localStorage.setItem("EventSupplies",jsonSupplies);
+			window.localStorage.setItem("EventFoods",jsonFoods);
+			window.localStorage.setItem("EventServices",jsonServices);
 			self.ErrorMessage(null);
 		}
 		catch (ex){
--- js/viewmodel.event.js
+++ js/viewmodel.event.js
@@ -5,9 +5,29 @@ function EventViewModel(){
 	self.ErrorMessage = ko.observable();
 	//public arrays
 	self.Events = ko.observableArray([]);
+	self.Guests = ko.observableArray([]);
+	self.Supplies = ko.observableArray([]);
+	self.Foods = ko.observableArray([]);
+	self.Services = ko.observableArray([]);
 	//private methods
 	var DisposeRelatedItems = function(eventID){
-		
+		var guestsToDel = [], foodsToDel = [], suppliesToDel = [], servicesToDel = [];
+		ko.utils.arrayForEach(self.Guests(), function(item) {
+			if(guest.EventID() == eventID) guestsToDel.push(item);
+		});
+		ko.utils.arrayForEach(self.Supplies(), function(item) {
+			if(guest.EventID() == eventID) suppliesToDel.push(item);
+		});
+		ko.utils.arrayForEach(self.Foods(), function(item) {
+			if(guest.EventID() == eventID) foodsToDel.push(item);
+		});
+		ko.utils.arrayForEach(self.Services(), function(item) {
+			if(guest.EventID() == eventID) servicesToDel.push(item);
+		});
+		ko.utils.arrayForEach(guestsToDel, function(toDel) { self.Guests.remove(toDel); });
+		ko.utils.arrayForEach(suppliesToDel, function(toDel) { self.Supplies.remove(toDel); });
+		ko.utils.arrayForEach(foodsToDel, function(toDel) { self.Foods.remove(toDel); });
+		ko.utils.arrayForEach(servicesToDel, function(toDel) { self.Services.remove(toDel); });
 	};
 	//public methods
 	self.getCategoryName = function(categoryID){
@@ -27,8 +47,9 @@ function EventViewModel(){
 	self.RemoveEvent = function(pEvent) {
 		var _delete = confirm("Are you sure you want to delete the event \"" + pEvent.EventName() + "\"?");
 		if(_delete){
-			var eventname = pEvent.EventName();
+			var eventname = pEvent.EventName(), eventID = pEvent.EventID();
 			self.Events.remove(pEvent);
+			DisposeRelatedItems(eventID);
 			self.UpdateLocalStorage();
 			Materialize.toast(eventname + ' has been deleted', 3000);
 		}
@@ -41,8 +62,24 @@ function EventViewModel(){
 	self.LoadFromStorage = function () {
 		var parsedEvents = JSON.parse(window.localStorage.getItem('ProjectEvents')) || [];
 		var parsedSelectedEvent = JSON.parse(window.localStorage.getItem('ProjectSelectedEvent')) || {};
+		var parsedGuests = JSON.parse(window.localStorage.getItem('EventGuests')) || [];
+		var parsedSupplies = JSON.parse(window.localStorage.getItem('EventSupplies')) || [];
+		var parsedFoods = JSON.parse(window.localStorage.getItem('EventFoods')) || [];
+		var parsedServices = JSON.parse(window.localStorage.getItem('EventServices')) || [];
 		var mappedEvents = $.map(parsedEvents, function(pEvent) {
 			return new ProjectEvent(pEvent);
+		});
+		var mappedGuests = $.map(parsedGuests, function(guest){
+			return new Guest(guest);
+		});
+		var mappedSupplies = $.map(parsedSupplies, function(supply){
+			return new Supply(supply);
+		});
+		var mappedFoods = $.map(parsedFoods, function(food){
+			return new Food(food);
+		});
+		var mappedServices = $.map(parsedServices, function(service){
+			return new Service(service);
 		});
 		var tmpSelectedEvent = new ProjectEvent(parsedSelectedEvent);
 		self.Events(mappedEvents);
@@ -51,13 +88,25 @@ function EventViewModel(){
 				self.SelectedEvent(tmpSelectedEvent);
 			}
 		});
+		self.Guests(mappedGuests);
+		self.Supplies(mappedSupplies);
+		self.Foods(mappedFoods);
+		self.Services(mappedServices);
 	};
 	self.UpdateLocalStorage = function() {
 		try {
 			var jsonEvents = ko.toJSON(self.Events());
 			var jsonSelectedEvent = ko.toJSON(self.SelectedEvent());
+			var jsonGuests = ko.toJSON(self.Guests());
+			var jsonSupplies = ko.toJSON(self.Supplies());
+			var jsonFoods = ko.toJSON(self.Foods());
+			var jsonServices = ko.toJSON(self.Services());
 			window.localStorage.setItem('ProjectEvents',jsonEvents);
 			window.localStorage.setItem('ProjectSelectedEvent',jsonSelectedEvent);
+			window.localStorage.setItem("EventGuests",jsonGuests);
+			window.localStorage.setItem("EventSupplies",jsonSupplies);
+			window.localStorage.setItem("EventFoods",jsonFoods);
+			window.localStorage.setItem("EventServices",jsonServices);
 			self.ErrorMessage(null);
 		} 
 		catch (ex) {
