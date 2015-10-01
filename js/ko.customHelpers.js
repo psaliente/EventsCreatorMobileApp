ba682a20059bc12212c4a4528d6d440e40160fcb
PSaliente <peterjohncorderosaliente@gmail.com>
2015-10-01 10:55:17 +0800

    applied linting

--- js/ko.customHelpers.js
+++ js/ko.customHelpers.js
@@ -1,9 +1,11 @@
+/*jslint plusplus: true*/
+/*jslint nomen: true*/
+/*global ko*/
 //http://jsfiddle.net/rniemeyer/jjbhy/
 ko.bindingHandlers.uniqueId = {
-    init: function(element, valueAccessor) {
+    init: function (element, valueAccessor) {
         var value = valueAccessor();
         value.id = value.id || ko.bindingHandlers.uniqueId.prefix + (++ko.bindingHandlers.uniqueId.counter);
-
         element.id = value.id;
     },
     counter: 0,
@@ -11,32 +13,31 @@ ko.bindingHandlers.uniqueId = {
 };
 
 ko.bindingHandlers.uniqueFor = {
-    init: function(element, valueAccessor) {
+    init: function (element, valueAccessor) {
         var value = valueAccessor();
         value.id = value.id || ko.bindingHandlers.uniqueId.prefix + (++ko.bindingHandlers.uniqueId.counter);
         
         element.setAttribute("for", value.id);
-    } 
+    }
 };
 
 ko.extenders.numeric = function (target, precision) {
 	var _result = ko.pureComputed({
 		read: target,
-		write: function(newValue){
+		write: function (newValue) {
 			var current = target(),
-			roundingMultiplier = Math.pow(10, precision),
-			newValueAsNum = isNaN(newValue) ? current : parseFloat(+newValue),
-			valueToWrite = Math.round(newValueAsNum * roundingMultiplier) / roundingMultiplier;
-			if(valueToWrite !== current){
+                roundingMultiplier = Math.pow(10, precision),
+                newValueAsNum = isNaN(newValue) ? current : parseFloat(+newValue),
+                valueToWrite = Math.round(newValueAsNum * roundingMultiplier) / roundingMultiplier;
+			if (valueToWrite !== current) {
 				target(valueToWrite);
-			}
-			else {
-				if(newValue !== current){
+			} else {
+				if (newValue !== current) {
 					target.notifySubscribers(valueToWrite);
 				}
 			}
 		}
 	}).extend({notify: 'always'});
 	_result(target());
-	return _result
+	return _result;
 };
--- js/viewmodel.index.js
+++ js/viewmodel.index.js
@@ -1,7 +1,12 @@
-function IndexViewModel(){
+/*jslint plusplus: true*/
+/*jslint nomen: true*/
+/*global ko,PreSelectedValues, $, Supply, Food, Service, Guest*/
+function IndexViewModel() {
+    "use strict";
 	//private properties
-	var self = this;
-	var IDCounter = 0;
+	var self = this,
+        IDCounter = 0,
+        setInitialList;
 	//public properties
 	self.EventName = ko.observable();
 	self.EventDate = ko.observable();
@@ -9,37 +14,36 @@ function IndexViewModel(){
 	self.Venue = ko.observable();
 	self.ErrorMessage = ko.observable();
 	//public arrays
-	self.Categories = [{id:"1", name:"Baby Shower"},
-              { id:"2", name:"Christening"},
-              { id:"3", name:"Birthday"},
-              { id:"4", name:"Wedding"},
-              { id:"5", name:"Funeral"},
-              { id:"6", name:"Reunion"},
-              { id:"7", name:"Conference"}];	
+	self.Categories = [{id: "1", name: "Baby Shower"},
+              { id: "2", name: "Christening"},
+              { id: "3", name: "Birthday"},
+              { id: "4", name: "Wedding"},
+              { id: "5", name: "Funeral"},
+              { id: "6", name: "Reunion"},
+              { id: "7", name: "Conference"}];
 	self.Events = ko.observableArray([]);
 	self.Supplies = ko.observableArray([]);
 	self.Foods = ko.observableArray([]);
 	self.Services = ko.observableArray([]);
 	//private methods
-	var SetInitialList = function (eventID) {
-		try{
-			var _id = parseInt(self.Category()) - 1;
-			ko.utils.arrayForEach(PreSelectedValues[_id]["InitialSupplies"], function(supply) {
-				var tmpSupply = new Supply($.extend({},supply,{EventID: eventID}));
+	setInitialList = function (eventID) {
+		try {
+			var _id = parseInt(self.Category(), 10) - 1;
+			ko.utils.arrayForEach(PreSelectedValues[_id].InitialSupplies, function (supply) {
+				var tmpSupply = new Supply($.extend({}, supply, {EventID: eventID}));
 				self.Supplies.push(tmpSupply);
 			});
-			ko.utils.arrayForEach(PreSelectedValues[_id]["InitialFoods"], function(food) {
-				var tmpFood = new Food($.extend({},food,{EventID: eventID}));
+			ko.utils.arrayForEach(PreSelectedValues[_id].InitialFoods, function (food) {
+				var tmpFood = new Food($.extend({}, food, {EventID: eventID}));
 				self.Foods.push(tmpFood);
 			});
-			ko.utils.arrayForEach(PreSelectedValues[_id]["InitialServices"], function(service) {
-				var tmpService = new Supply($.extend({},service,{EventID: eventID}));
+			ko.utils.arrayForEach(PreSelectedValues[_id].InitialServices, function (service) {
+				var tmpService = new Supply($.extend({}, service, {EventID: eventID}));
 				self.Services.push(tmpService);
 			});
 			self.ErrorMessage(null);
-		} 
-		catch (ex) { 
-			self.ErrorMessage("Error! " + ex + ", please contact developer."); 
+		} catch (ex) {
+			self.ErrorMessage("Error! " + ex + ", please contact developer.");
 		}
 	};
 	//public methods
@@ -52,52 +56,51 @@ function IndexViewModel(){
 			Venue: self.Venue()
 		};
 		self.Events.push(tmpEvent);
-		SetInitialList(tmpEvent.EventID);
+		setInitialList(tmpEvent.EventID);
 		self.UpdateLocalStorage();
 	};
 	self.LoadFromStorage = function () {
-		IDCounter = parseInt(window.localStorage.getItem('ProjectEventIDCounter')) || 0;
-		var parsedEvents = JSON.parse(window.localStorage.getItem('ProjectEvents')) || [];
-		var parsedSupplies = JSON.parse(window.localStorage.getItem('EventSupplies')) || [];
-		var parsedFoods = JSON.parse(window.localStorage.getItem('EventFoods')) || [];
-		var parsedServices = JSON.parse(window.localStorage.getItem('EventServices')) || [];
-		var mappedSupplies = $.map(parsedSupplies, function(supply){
-			return new Supply(supply);
-		});
-		var mappedFoods = $.map(parsedFoods, function(food){
-			return new Food(food);
-		});
-		var mappedServices = $.map(parsedServices, function(service){
-			return new Service(service);
-		});
+		IDCounter = parseInt(window.localStorage.getItem('ProjectEventIDCounter'), 10) || 0;
+		var parsedEvents = JSON.parse(window.localStorage.getItem('ProjectEvents')) || [],
+            parsedSupplies = JSON.parse(window.localStorage.getItem('EventSupplies')) || [],
+            parsedFoods = JSON.parse(window.localStorage.getItem('EventFoods')) || [],
+            parsedServices = JSON.parse(window.localStorage.getItem('EventServices')) || [],
+            mappedSupplies = $.map(parsedSupplies, function (supply) {
+                return new Supply(supply);
+            }),
+            mappedFoods = $.map(parsedFoods, function (food) {
+                return new Food(food);
+            }),
+            mappedServices = $.map(parsedServices, function (service) {
+                return new Service(service);
+            });
 		self.Events(parsedEvents);
 		self.Supplies(mappedSupplies);
 		self.Foods(mappedFoods);
 		self.Services(mappedServices);
 	};
-	self.UpdateLocalStorage = function(){
-		try{
-			var jsonData = ko.toJSON(self.Events());
-			var jsonSupplies = ko.toJSON(self.Supplies());
-			var jsonFoods = ko.toJSON(self.Foods());
-			var jsonServices = ko.toJSON(self.Services());
-			window.localStorage.setItem('ProjectEvents',jsonData);
-			window.localStorage.setItem('ProjectEventIDCounter',IDCounter);
-			window.localStorage.setItem("EventSupplies",jsonSupplies);
-			window.localStorage.setItem("EventFoods",jsonFoods);
-			window.localStorage.setItem("EventServices",jsonServices);
+	self.UpdateLocalStorage = function () {
+		try {
+			var jsonData = ko.toJSON(self.Events()),
+                jsonSupplies = ko.toJSON(self.Supplies()),
+                jsonFoods = ko.toJSON(self.Foods()),
+                jsonServices = ko.toJSON(self.Services());
+			window.localStorage.setItem('ProjectEvents', jsonData);
+			window.localStorage.setItem('ProjectEventIDCounter', IDCounter);
+			window.localStorage.setItem("EventSupplies", jsonSupplies);
+			window.localStorage.setItem("EventFoods", jsonFoods);
+			window.localStorage.setItem("EventServices", jsonServices);
 			self.EventName("");
 			self.EventDate("");
 			self.Category("");
 			self.Venue("");
 			self.ErrorMessage(null);
-		}
-		catch (ex){
-			self.ErrorMessage("Error! " + ex + ", please contact developer."); 
+		} catch (ex) {
+			self.ErrorMessage("Error! " + ex + ", please contact developer.");
 		}
 	};
 }
 
 var ivm = new IndexViewModel();
-$(document).ready(function(){ko.applyBindings(ivm);ivm.LoadFromStorage()});
+$(document).ready(function () { 'use strict'; ko.applyBindings(ivm); ivm.LoadFromStorage(); });
 
